67eed322cedf4268a49c6855e3ea6cd23693aac9
AlvarezAriel <alvarez.ariel.work@gmail.com>
2016-05-01 00:53:47 -0300

    hiden token

--- travis-runner.sh
+++ travis-runner.sh
@@ -21,7 +21,7 @@ then
     git add --all
     git commit -m "Automatic bower module deploy"
     git remote add origin https://$GH_TOKEN@github.com/AlvarezAriel/gs-element-starter.git
-    git push origin master --force
+    git push origin master --force --quiet origin master > /dev/null 2>&1
   }
 
   deploy_gh_master
